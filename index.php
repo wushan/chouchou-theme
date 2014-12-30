ea15449c7447e990189ac93162968d2f7d88a4fc
tonychiang <chiangloveslynn@gmail.com>
2014-12-31 01:26:13 +0800

    no message

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 			<div id="content">
 
 				<div id="inner-content" class="wrap cf">
-            <?php get_sidebar('sidebar-left'); ?>
+	            <?php get_sidebar('sidebar-left'); ?>
 						<main id="main" class="main-wrapper" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
--- post-formats/format.php
+++ post-formats/format.php
@@ -26,7 +26,7 @@
                        /* the time the post was published */
                        '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
                        /* the author of the post */
-                       '<span class="by">by</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+                       '<span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
                     ); ?>
 
                   </p>
